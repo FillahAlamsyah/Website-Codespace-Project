97ecd795f6c47f1c7bcf1ca22a766cd0b1025721
Fillah Alamsyah <87614211+FillahAlamsyah@users.noreply.github.com>
2024-09-24 09:56:00 +0000

    Add Images And Update Contents

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Fillah Alamsyah",
+  title: "Instrumentation Physicist",
+  email: "fillahalamsyah@gmail.com",
+  gitHub: "FillahAlamsyah",
+  instagram: "fillah_alamsyah",
+  linkedIn: "fillah-alamsyah",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "fillahalamsyah2885",
 };
 
 const primaryColor = "#4E567E";
